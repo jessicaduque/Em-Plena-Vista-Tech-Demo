1e43fe9ce93a819a96fae7ca79063834e56e4335
Jessica <jduquedeveloper@gmail.com>
2024-06-10 19:52:30 -0300

    Material finish / Comments Start

--- Assets/Game/Scripts/InteractionSystem/IInteractable.cs
+++ Assets/Game/Scripts/InteractionSystem/IInteractable.cs
@@ -1,9 +1,22 @@
 using System.Collections;
-
+/// <summary>
+/// Interface for objects that are interactable to the player
+/// </summary>
 public interface IInteractable
-{ 
+{
     public string interactionPrompt { get; }
+    /// <summary>
+    /// Checks if object's specific conditions are met for the player to be able to interact with it.
+    /// </summary>
+    /// <returns>Returns if interactor can interact with object.</returns>
     public bool CanInteract();
+    /// <summary>
+    /// Object's intended interaction is executed.
+    /// </summary>
+    /// <param name="interactor">Interactor that's trying to interact with object.</param>
     public void InteractControl(Interactor interactor);
+    /// <summary>
+    /// Makes interactor's object turn towards the interactable object.
+    /// </summary>
     public IEnumerator TurnToInteractable();
 }
