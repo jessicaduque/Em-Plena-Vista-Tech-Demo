9979e5182d8efa34f9c72e0ed176d128fc56d0fe
Jessica <jduquedeveloper@gmail.com>
2024-06-17 19:19:11 -0300

    End and Pause progress / Falling bug almost fixed

--- Assets/Game/Scripts/Player/ThirdPersonAnimation.cs
+++ Assets/Game/Scripts/Player/ThirdPersonAnimation.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Utils.Singleton;
 
@@ -6,6 +7,7 @@ public class ThirdPersonAnimation : Singleton<ThirdPersonAnimation>
     private Animator _animator;
     private Rigidbody _rb;
     private float _maxSpeed;
+    private bool _isGrounded = true;
 
     private new void Awake()
     {
@@ -18,11 +20,47 @@ public class ThirdPersonAnimation : Singleton<ThirdPersonAnimation>
         _maxSpeed = GetComponent<ThirdPersonController>().maxRunSpeed;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        _animator.SetFloat("speed", _rb.velocity.magnitude / _maxSpeed);
+        if (_isGrounded)
+            _animator.SetFloat("speed", _rb.velocity.magnitude / _maxSpeed);
+    }
+    
+    #region Trigger colliders
+    private void OnTriggerEnter(Collider other)
+    {
+        StopAllCoroutines();
+        SetBool("Falling", false);
+        _isGrounded = true;
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if(!_isGrounded)
+        {
+            StopAllCoroutines();
+            SetBool("Falling", false);
+            _isGrounded = true;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StartCoroutine(MinTimeForNotGrounded());
+    }
+    #endregion
+
+    private IEnumerator MinTimeForNotGrounded()
+    {
+        _isGrounded = false;
+        yield return new WaitForSeconds(0.2f);
+
+        SetBool("Falling", true);
+    }
+
+
+    #region Set Animation Parameters
     public void SetTrigger(string triggerName)
     {
         _rb.velocity = Vector3.zero;
@@ -34,4 +72,5 @@ public class ThirdPersonAnimation : Singleton<ThirdPersonAnimation>
         _rb.velocity = Vector3.zero;
         _animator.SetBool(boolName, state);
     }
+    #endregion
 }
