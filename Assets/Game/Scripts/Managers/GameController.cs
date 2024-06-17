bc61042d7176f923fc1efc9580da58fd821e4c2b
Jessica <jduquedeveloper@gmail.com>
2024-06-17 01:18:01 -0300

    Controlls better recognition with events

--- Assets/Game/Scripts/Managers/GameController.cs
+++ Assets/Game/Scripts/Managers/GameController.cs
@@ -1,10 +1,34 @@
 using Utils.Singleton;
+using UnityEngine.InputSystem;
+using System;
 
 /// <summary>
 /// Overall controller instance for the whole tech demo
 /// </summary>
 public class GameController : Singleton<GameController>
 {
+    public event Action _gamepadConnected; // Event for when gamepad is connected
+    public event Action _gamepadDisconnected; // Event for when gamepad is disconnected
     public bool gameCompleted { get; private set; } // Indicates if all puzzles have been completed and player has reached the end cave
+
+    private void Start()
+    {
+        InputSystem.onDeviceChange += onInputDeviceChange;
+    }
+
+    #region Devices
+    public void onInputDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        switch (change)
+        {
+            case InputDeviceChange.Added:
+                _gamepadConnected?.Invoke();
+                break;
+            case InputDeviceChange.Removed:
+                _gamepadDisconnected?.Invoke();
+                break;
+        }
+    }
+    #endregion
 
 }
